c568af848a1771b5df301df0dbb3d084d5099df6
Yuki Soejima <zimazima2442@gmail.com>
2024-03-15 15:00:58 +0900

    vueメモナビバーまで

---
+++ src/components/MainPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-page">
+    <div class="left-menu">
+      左メニュー
+    </div>
+    <div class="right-view">
+      右ビュー
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.main-page {
+  padding: 10px;
+}
+</style>
