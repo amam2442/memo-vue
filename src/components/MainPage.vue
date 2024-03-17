1511c80dda4eedbb25302a5175c97853b6412b92
Yuki Soejima <zimazima2442@gmail.com>
2024-03-17 10:19:35 +0900

    削除ボタン機能を追加

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="main-page">
-    <div class="left-menu">
+    <div class="left-menu" @click.self="onEditNoteEnd()">
        <!-- ノートリスト -->
        <div class="note" v-for="note in noteList" v-bind:key="note.id">
         <div class="note-icon">
           <i class="fas fa-file-alt"></i>
         </div>
         <div class="note-name">{{note.name}}</div>
+        <div class="button-icon" @click="onClickDelete(note)">
+        <i class="fas fa-trash"></i>
+      </div>
       </div>
       <!-- ノート追加ボタン -->
       <button class="transparent" @click="onClickButtonAdd">
         <i class="fas fa-plus-square"></i>ノートを追加
       </button>
     </div>
-    <div class="right-view">
+    <div class="right-view" @click.self="onEditNoteEnd()">
       右ビュー
     </div>
   </div>
@@ -33,6 +36,20 @@ export default {
         id : new Date().getTime().toString(16),
         name : `新規ノート`,
       })
+    },
+    onClickDelete : function(deleteNote) {
+      const index = this.noteList.indexOf(deleteNote);
+      this.noteList.splice(index, 1);
+    },
+    onEditNoteStart : function(editNote) {
+      for (let note of this.noteList) {
+        note.editing = (note.id === editNote.id);
+      }
+    },
+    onEditNoteEnd : function() {
+      for (let note of this.noteList) {
+          note.editing = false;
+      }
     },
   },
 }
