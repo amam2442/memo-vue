54dbb0ad0ceb151a03599f68d873a63592a9b825
Yuki Soejima <zimazima2442@gmail.com>
2024-03-22 13:56:42 +0900

    削除機能を追加’

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="main-page">
     <div class="left-menu">
-       <!-- ノートリスト -->
-       <div class="note" v-for="note in noteList" v-bind:key="note.id">
+      <!-- ノートリスト -->
+      <div class="note" v-for="note in noteList" v-bind:key="note.id">
         <div class="note-icon">
           <i class="fas fa-file-alt"></i>
         </div>
-        <div class="note-name">{{note.name}}</div>
+        <div class="note-name">{{ note.name }}</div>
+        <div class="button-icon">
+          <i class="fas fa-edit"></i>
+        </div>
+        <div class="button-icon" @click="onDeleteNote">
+          <i class="fas fa-trash"></i>
+        </div>
       </div>
       <!-- ノート追加ボタン -->
       <button class="transparent" @click="onClickButtonAdd">
@@ -24,15 +30,20 @@
 export default {
   data() {
     return {
-      noteList : [],
+      noteList: [],
     }
   },
   methods: {
-    onClickButtonAdd : function() {
+    onClickButtonAdd: function () {
       this.noteList.push({
-        id : new Date().getTime().toString(16),
-        name : `新規ノート`,
+        id: new Date().getTime().toString(16),
+        name: `新規ノート`,
       })
+    },
+
+    onDeleteNote: function (deleteNote) {
+      const index = this.noteList.indexOf(deleteNote);
+      this.noteList.splice(index, 1);
     },
   },
 }
@@ -50,22 +61,36 @@ export default {
 }
 
 .note {
-      margin: 10px 0;
-      display: flex;
-      align-items: center;
-      padding: 5px;
-      color: rgba(25, 23, 17, 0.6);
-      .note-icon {
-        margin-left: 10px;
-      }
-      .note-name {
-        width: 100%;
-        padding: 3px 10px;
-      }
-    }
+  margin: 10px 0;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  color: rgba(25, 23, 17, 0.6);
+
+  .note-icon {
+    margin-left: 10px;
+  }
+
+  .note-name {
+    width: 100%;
+    padding: 3px 10px;
+  }
+}
+
 .right-view {
   flex-grow: 1;
   padding: 10px;
 }
 
+
+.buttons {
+  display: flex;
+  flex-direction: row;
+}
+
+.button-icon {
+  padding: 3px;
+  margin-left: 3px;
+  border-radius: 5px;
+}
 </style>
